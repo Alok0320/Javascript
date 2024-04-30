9745a198c998ab3184387f023040055b67a46886
Alok Upadhyay <alokupadhyay0188@gmail.com>
2024-05-01 02:30:23 +0530

    New Learnings

---
+++ 05_iterations/four.js
@@ -0,0 +1,37 @@
+// For in loop
+// Objects k upr for in and arrays k upr for of except some cases
+
+const myObject = {
+    js: 'javascript',
+    cpp: 'C++',
+    rb: 'ruby',
+    swift: 'swift by apple'
+}
+
+for (const key in myObject) {
+    // console.log(`${key} shortcut is for ${myObject[key]}`);
+}
+
+const programming = ["js", "rb", "py", "java", "cpp"]
+
+for (const key in programming) {
+    // console.log(key);
+}
+
+for (const key in programming) {
+   // console.log(programming[key]);
+}
+
+// Arrays ki by default keys uski index value hi hoti h e.g 0,1,2,3,4,,
+// Object mein hm manually key dal skte h 
+
+
+const map = new Map()
+map.set('IN', "India")
+map.set('USA', "United States of America")
+map.set('Fr', "France")
+map.set('IN', "India")
+
+for (const key in map) { // Not iterable here 
+    console.log(key);
+}
